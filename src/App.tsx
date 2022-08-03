c8933cc06a5a8fa4ef7a4f0f699ef801de0f79a9
zhangyang <2224143549@qq.com>
2022-08-03 23:49:24 +0800

    feat: 增加登录路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import Login from '@/pages/Login'
+import { Navigate, Route, Routes } from 'react-router'
+import { HashRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Navigate to={'/login'} />} />
+        <Route path={'/login'} element={<Login />} />
+      </Routes>
+    </Router>
+    // <div className="app">
+    //   <Login />
+    // </div>
   )
 }
 
